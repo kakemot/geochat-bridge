62cd24db6c20544fac58b8469a2fd0453ee6d945
kakemot <35057409+kakemot@users.noreply.github.com>
2024-03-04 21:09:27 +0100

    Server gif support websocket-bridge.js

--- websocket-bridge.js
+++ websocket-bridge.js
@@ -5,13 +5,13 @@ const wss = new WebSocket.Server({ port: port, host: host, path: '/chat' });
 const clients = new Map(); // Use a Map to associate clients with locations
 const messagesByCity = new Map(); // Store messages by city
 
-function storeMessage(city, username, locality, content, timestamp) {
+function storeMessage(city, username, locality, content, timestamp, gifUrl) {
     if (!messagesByCity.has(city)) {
         messagesByCity.set(city, []);
     }
     let messages = messagesByCity.get(city);
     // Store both username and content in each message
-    messages.push({ username, content, locality, timestamp });
+    messages.push({ username, content, locality, timestamp, gifUrl });
     // Keep only the last 10 messages for the city
     if (messages.length > 10) {
         messages = messages.slice(-10);
@@ -19,24 +19,24 @@ function storeMessage(city, username, locality, content, timestamp) {
     messagesByCity.set(city, messages);
 }
 
-function broadcastMessage(username, content, locality, senderWs, senderLocation, timestamp) {
+function broadcastMessage(username, content, locality, senderWs, senderLocation, timestamp, gifUrl) {
     const senderCity = senderLocation.city;
     clients.forEach((location, client) => {
         if (client.readyState === WebSocket.OPEN && client !== senderWs) {
             const clientCity = location.city;
             if (clientCity === senderCity) {
                 // Send both username and content as a JSON string
-                client.send(JSON.stringify({ username, content, locality, timestamp }));
+                client.send(JSON.stringify({ username, content, locality, timestamp, gifUrl }));
             }
         }
     });
 }
 
-function sendLastMessages(client, city, locality, timestamp) {
+function sendLastMessages(client, city, locality) {
     const messages = messagesByCity.get(city) || [];
-    messages.forEach(({ username, content, locality, timestamp }) => {
+    messages.forEach(({ username, content, locality, timestamp, gifUrl }) => {
         // Send both username and content as a JSON string
-        client.send(JSON.stringify({ username, content, locality, timestamp }));
+        client.send(JSON.stringify({ username, content, locality, timestamp, gifUrl }));
     });
 }
 
@@ -47,15 +47,15 @@ wss.on('connection', function connection(ws) {
             const city = message.city;
             clients.set(ws, { city: message.city });
             console.log("send last messages in " + city)
-            sendLastMessages(ws, city, message.locality, message.timestamp); // Send last 10 messages for this city
+            sendLastMessages(ws, city, message.locality); // Send last 10 messages for this city
         } else if (message.type === 'chat') {
             console.log('received: %s', message.content);
             const senderLocation = clients.get(ws);
             if (senderLocation) {
                 const senderCity = senderLocation.city;
                 // Assuming 'username' is part of the chat message structure
-                storeMessage(senderCity, message.username, message.locality, message.content, message.timestamp); // Store the message with username under the sender's city
-                broadcastMessage(message.username, message.content, message.locality, ws, senderLocation, message.timestamp); // Pass the WebSocket instance directly
+                storeMessage(senderCity, message.username, message.locality, message.content, message.timestamp, message.gifUrl); // Store the message with username under the sender's city
+                broadcastMessage(message.username, message.content, message.locality, ws, senderLocation, message.timestamp, message.gifUrl); // Pass the WebSocket instance directly
             }
         }
     });
